e2e6b2d8a131cfe51f8aea334d266ed0c9b51d34
md-ashik-max <ar1214697@gmail.com>
2024-05-12 09:36:00 +0600

    added post method for recive data from client site and post on database

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const database = client.db("assignmentDB");
+        const assignmentCollection = database.collection("assignment");
+
+
+        app.post('/assignment', async (req, res) => {
+            const newAssignment = req.body;
+            console.log(newAssignment)
+            const result = await assignmentCollection.insertOne(newAssignment)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
