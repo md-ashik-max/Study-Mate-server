a7e75c09f429ee8ab4e323339d9441fb8789b932
md-ashik-max <ar1214697@gmail.com>
2024-05-12 19:28:17 +0600

    added delete method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,12 +32,35 @@ async function run() {
         const assignmentCollection = database.collection("assignment");
 
 
+         app.get('/assignment',async(req,res)=>{
+            const cursor = assignmentCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+          })
+          app.get('/assignment/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:new ObjectId(id)}
+            const result =await assignmentCollection.findOne(query)
+            res.send(result)
+        }) 
+
+
         app.post('/assignment', async (req, res) => {
             const newAssignment = req.body;
             console.log(newAssignment)
             const result = await assignmentCollection.insertOne(newAssignment)
             res.send(result)
         })
+
+        app.delete('/assignment/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result =await assignmentCollection.deleteOne(query)
+            res.send(result)
+      
+          })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
