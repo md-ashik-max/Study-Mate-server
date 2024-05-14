ad1e189ef828ad4cea0d30fb1dbdd2c5cfa57f09
md-ashik-max <ar1214697@gmail.com>
2024-05-14 15:12:46 +0600

    create get method for load data by email for impelement jwt

--- index.js
+++ index.js
@@ -31,6 +31,27 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middleware
+const logger = (req, res, next) => {
+    console.log('log:info', req.method, req.url)
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    console.log('token from middleware', token)
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded;
+        next()
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -42,16 +63,16 @@ async function run() {
 
         // auth related api
 
-        app.post('/jwt', async (req, res) => {
+        app.post('/jwt', logger, async (req, res) => {
             const user = req.body;
-            console.log('user for token', user);
+            console.log('user for token', user?.email);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.cookie('token', token, {
-                httpOnly:true,
-                secure:true,
-                sameSite:'none'
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
             })
-            .send({ success: true })
+                .send({ success: true })
         })
 
         app.post('/logout', async (req, res) => {
@@ -115,7 +136,17 @@ async function run() {
 
         app.get('/submitted', async (req, res) => {
             const cursor = submittedCollection.find();
-            console.log('cookies',req.cookies)
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/submitted/:email', logger, verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (req.user.email !== email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email };
+            const cursor = submittedCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
         })
